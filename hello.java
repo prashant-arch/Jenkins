e3acd568ce954c29ae6d3272005222b75b7dd869
prashant <prashantnagpal.nagpal@gmail.com>
2020-03-10 20:41:49 +0530

    added hello world program

---
+++ hello.java
@@ -0,0 +1,9 @@
+
+public class hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Prashant Nagpal Welcome to Jenkins this project is used to integrate jenkins with github.");
+
+	}
+
+}
