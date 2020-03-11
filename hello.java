1a0c4321ecdc67951f11b8c4bd335a22ef5cdfa4
prashant <prashantnagpal.nagpal@gmail.com>
2020-03-11 09:27:43 +0530

    Changes Made in the file

--- hello.java
+++ hello.java
@@ -1,8 +1,7 @@
-
 public class hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Prashant Nagpal Welcome to Jenkins this project is used to integrate jenkins with github.");
+		System.out.println("Hello Hello Prashant Nagpal Welcome to Jenkins this project is used to integrate jenkins with github.");
 
 	}
 
